9bb6348431bbec727615bc498edc5d555e4a930d
穆小康 <muxiaokang@olmail.org>
2018-11-14 08:47:12 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -11,3 +11,9 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+//对帖子内容进行摘要，SEO
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
