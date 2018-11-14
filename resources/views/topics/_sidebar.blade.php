57fce227ba2c3250c405e19947c58fef27aac8b8
穆小康 <muxiaokang@olmail.org>
2018-11-14 07:06:09 +0000

    Topic index page

---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Owner
+ * Date: 2018/11/14
+ * Time: 11:52
+ */
+?>
+<div class="panel panel-default">
+    <div class="panel-body">
+        右边导航栏
+    </div>
+</div>
