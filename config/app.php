dce67a0103aa2c763b598a49896d08b115a682aa
穆小康 <muxiaokang@olmail.org>
2018-11-26 09:49:32 +0000

    Admin can manage users

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://larabbs.test'),
+    'url' => env('APP_URL', 'http://larabbs.com'),
 
     /*
     |--------------------------------------------------------------------------
